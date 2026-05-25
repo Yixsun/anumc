ef031f00fb745fefaad18f537626a54556c1c586
Yixia Sun <sunyixia025@gmail.com>
2026-05-25 12:05:11 +1000

    initial springboot setup

---
+++ src/main/kotlin/org/example/anumc_website/AnumcWebsiteApplication.kt
@@ -0,0 +1,11 @@
+package org.example.anumc_website
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnumcWebsiteApplication
+
+fun main(args: Array<String>) {
+    runApplication<AnumcWebsiteApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/anumc_website/AnumcWebsiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.anumc_website
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnumcWebsiteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
